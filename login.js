84641286fa749e77d7ded0422055bf22c2cca4e1
Eric WH Huang <vuarnet0318@gmail.com>
2021-06-20 12:31:52 +0800

    feat: set axios for LogIn

--- login.js
+++ login.js
@@ -1,1 +1,20 @@
-console.log("Login!!");
+//=== URL Settings ===
+const BASE_URL = "https://vue3-course-api.hexschool.io";
+const API_PATH = "hello-eric";
+const Login_API = `${BASE_URL}/admin/signin`;
+
+//=== Event Func ===
+function handleLogIn() {
+  const username = document.querySelector("#username").value;
+  const password = document.querySelector("#password").value;
+
+  console.log("===>", Login_API);
+  axios
+    .post(Login_API, { username, password })
+    .then((res) => console.log("success get:", res))
+    .catch((rej) => console.log("failed:", rej));
+}
+
+//=== Get Loging Dom and set callback event ===
+const btnLogIn = document.querySelector("#login");
+btnLogIn.addEventListener("click", handleLogIn);
--- processOrder.js
+++ processOrder.js
@@ -1,1 +1,6 @@
+//=== URL Settings ===
+// const PRODUCT_API = `${BASE_URL}api/${API_PATH}/products`;
+
 console.log("Process Order!!");
+
+// axios.get(PRODUCT_API).then((res) => console.log(PRODUCT_API, res));
