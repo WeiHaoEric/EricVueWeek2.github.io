ff462034747b65d9658b2ae46a4d5a1cc17d93c6
Eric WH Huang <vuarnet0318@gmail.com>
2021-07-01 16:52:47 +0800

    feat: refactor code to practice this

--- process.js
+++ process.js
@@ -7,99 +7,114 @@ const PRODUCT_API = `${BASE_URL}/api/${API_PATH}/admin/products`;
 const DELETE_ITEM_API = `${BASE_URL}/api/${API_PATH}/admin/product`; // api_path/admin/product/:product_id
 const ADD_PRODUCT_API = `${BASE_URL}/api/${API_PATH}/admin/product`; // api_path/admin/product
 
-//=== Handle Event Func ===
-function handleLogIn() {
-  const username = document.querySelector("#username").value;
-  const password = document.querySelector("#password").value;
+const EricApp = {
+  init() {
+    //=== Main Procedure ===
+    const btnLogIn = document.querySelector("#login");
+    const btnAddItem = document.querySelector("#add-item");
+    
+    btnLogIn.addEventListener("click", this.handleLogIn.bind(this));
+    btnAddItem.addEventListener("click", this.handleAddItem.bind(this));
 
-  axios
-    .post(Login_API, { username, password })
-    .then((res) => {
-      const { expired, token } = res.data;
-      document.cookie = `hexToken = ${token}`;
-      document.cookie = `expires = ${expired}`;
 
-      const loginInfo = document.querySelector(".login-info");
-      loginInfo.innerHTML = `<h3 style="color:green">順利登入，可操作以下的商品</h3>`;
+    this.checkToken();
+  },
 
-      assignTokenToAxios(token);
-    })
-    .catch((rej) => console.log("failed:", rej));
-}
+  //=== Handle Event Func ===
+  handleLogIn() {
+    const username = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
 
-function handleAddItem() {
-  // title(String)、category(String)、unit(String)、origin_price(Number)、price(Number) 為必填欄位
-  const productName = document.querySelector("#product");
-  const productNum = document.querySelector("#product-number");
-  const originalPrice = document.querySelector("#original-price");
-  const actualPrice = document.querySelector("#actual-price");
-  const enableSelect = document.querySelector(".enable-select");
-
-  const postData = {
-    data: {
-      title: productName.value,
-      category: "life",
-      unit: productNum.value,
-      origin_price: parseInt(originalPrice.value),
-      price: parseInt(actualPrice.value),
-      is_enabled: enableSelect.value,
-    },
-  };
-
-  axios
-    .post(ADD_PRODUCT_API, postData)
-    .then((res) => {
-      console.log("商品新增成功:", res);
-      render();
-    })
-    .catch((rej) => console.log("商品新增錯誤:", rej));
-}
-
-function handleDeleteItem(prdId) {
-  return () => {
     axios
-      .delete(`${DELETE_ITEM_API}/${prdId}`)
+      .post(Login_API, { username, password })
       .then((res) => {
-        console.log("成功刪除:", res);
-        render();
+        const { expired, token } = res.data;
+        // document.cookie = `hexToken = ${token};expires = ${new Date(expired)}`;
+        const expires = new Date(expired);
+        document.cookie = `hexToken=${token}; expires=${expires};`;
+
+        const loginInfo = document.querySelector(".login-info");
+        loginInfo.innerHTML = `<h3 style="color:green">順利登入，可操作以下的商品</h3>`;
+
+        console.log("===>this:", this);
+        this.assignTokenToAxios(token);
       })
-      .catch((rej) => {
-        console.log("失敗刪除:", rej);
-      });
-  };
-}
+      .catch((rej) => console.log("failed:", rej));
+  },
 
-// === Check Cookie has token and expires
-function assignTokenToAxios(newToken) {
-  axios.defaults.headers.common["Authorization"] = newToken;
+  handleAddItem() {
+    // title(String)、category(String)、unit(String)、origin_price(Number)、price(Number) 為必填欄位
+    const productName = document.querySelector("#product");
+    const productNum = document.querySelector("#product-number");
+    const originalPrice = document.querySelector("#original-price");
+    const actualPrice = document.querySelector("#actual-price");
+    const enableSelect = document.querySelector(".enable-select");
 
-  axios.post(USER_CHECK_API).then((res) => {
-    console.log("確認使用者是否登入:", res);
-    if (res.data.success) render();
-  });
-}
+    const postData = {
+      data: {
+        title: productName.value,
+        category: "life",
+        unit: productNum.value,
+        origin_price: parseInt(originalPrice.value),
+        price: parseInt(actualPrice.value),
+        is_enabled: enableSelect.value,
+      },
+    };
 
-function checkToken() {
-  const token = document.cookie.split(";")[0].split("=")[1];
-  const loginInfo = document.querySelector(".login-info");
-  loginInfo.innerHTML = token
-    ? `<h3 style="color:green">順利登入，可操作以下的商品</h3>`
-    : `<h3 style="color: rgba(244, 135, 135, 0.813)">請先登入</h3>`;
+    axios
+      .post(ADD_PRODUCT_API, postData)
+      .then((res) => {
+        console.log("商品新增成功:", res);
+        this.render();
+      })
+      .catch((rej) => console.log("商品新增錯誤:", rej));
+  },
 
-  if (token) assignTokenToAxios(token);
-}
+  handleDeleteItem(prdId) {
+    return () => {
+      axios
+        .delete(`${DELETE_ITEM_API}/${prdId}`)
+        .then((res) => {
+          console.log("成功刪除:", res);
+          this.render();
+        })
+        .catch((rej) => {
+          console.log("失敗刪除:", rej);
+        });
+    };
+  },
 
-//=== Handle Event Function ===
-function render() {
-  const idList = [];
-  axios.get(PRODUCT_API).then((res) => {
-    const table = document.querySelector("#productList");
-    const { success, products } = res.data;
+  // === Check Cookie has token and expires
+  assignTokenToAxios(newToken) {
+    axios.defaults.headers.common["Authorization"] = newToken;
 
-    let renderData = "";
-    products.forEach(
-      ({ title, origin_price, price, is_enabled, id: prdId }, idx) => {
-        renderData += `
+    axios.post(USER_CHECK_API).then((res) => {
+      console.log("確認使用者是否登入:", res);
+      if (res.data.success) this.render();
+    });
+  },
+
+  checkToken() {
+    const token = document.cookie.split(";")[0].split("=")[1];
+    const loginInfo = document.querySelector(".login-info");
+    loginInfo.innerHTML = token
+      ? `<h3 style="color:green">順利登入，可操作以下的商品</h3>`
+      : `<h3 style="color: rgba(244, 135, 135, 0.813)">請先登入</h3>`;
+
+    if (token) this.assignTokenToAxios(token);
+  },
+
+  //=== Handle Event , ===
+  render() {
+    const idList = [];
+    axios.get(PRODUCT_API).then((res) => {
+      const table = document.querySelector("#productList");
+      const { success, products } = res.data;
+
+      let renderData = "";
+      products.forEach(
+        ({ title, origin_price, price, is_enabled, id: prdId }, idx) => {
+          renderData += `
 			<tr>
 					<td width=${"120"} style="text-align:center">${title}</td>
 					<td width=${"120"} style="text-align:center">${origin_price}</td>
@@ -113,24 +128,18 @@ function render() {
 			</tr>
 	`;
 
-        idList.push(prdId);
-      }
-    );
+          idList.push(prdId);
+        }
+      );
 
-    table.innerHTML = success ? renderData : null;
+      table.innerHTML = success ? renderData : null;
 
-    const btnList = document.querySelectorAll("#btn-delete");
-    btnList.forEach((btn, idx) =>
-      btn.addEventListener("click", handleDeleteItem(idList[idx]))
-    );
-  });
-}
+      const btnList = document.querySelectorAll("#btn-delete");
+      btnList.forEach((btn, idx) =>
+        btn.addEventListener("click", this.handleDeleteItem(idList[idx]))
+      );
+    });
+  },
+};
 
-//=== Main Procedure ===
-const btnLogIn = document.querySelector("#login");
-btnLogIn.addEventListener("click", handleLogIn);
-
-const btnAddItem = document.querySelector("#add-item");
-btnAddItem.addEventListener("click", handleAddItem);
-
-checkToken();
+EricApp.init();
